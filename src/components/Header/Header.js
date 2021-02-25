461eace34e6ffd04e44e67bb48359de7b0da61ff
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-25 23:30:28 +0600

    add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is Header</h1>
+    <div className="header">
+      <img src={logo} alt=""/>
     </div>
   );
 };
