7ad3b52580bf096ebc76de66bada4d1b2536578a
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 00:09:20 +0600

    load 10 data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 
 const Shop = () => {
-  console.log(fakeData);
-  
+  const first10 = fakeData.slice(0,10);
+  const [products, setProducts] = useState(first10);
+
   return (
     <div>
       <h1>This is shop</h1>
+      <h3>{products.length}</h3>
     </div>
   );
 };
