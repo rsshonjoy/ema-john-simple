93df6f8a51d3c4ae9c45de66306f7e64cdad4ea7
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 00:01:15 +0600

    fakeData

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+const Shop = () => {
+  console.log(fakeData);
+  
+  return (
+    <div>
+      <h1>This is shop</h1>
+    </div>
+  );
+};
+
+export default Shop;
