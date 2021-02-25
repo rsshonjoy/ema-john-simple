e2ac245999b98e201766b4b8b58fff942f03f1da
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-25 23:45:42 +0600

    create navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
   return (
     <div className="header">
       <img src={logo} alt=""/>
+      <nav><a href="/shop">Shop</a>
+          <a href="/review">Order Review</a>
+          <a href="/manage">Manage Inventory</a>
+        </nav>
     </div>
   );
 };
