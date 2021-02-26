7e194229bc80292505e8c9e6848e792766944f56
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 19:26:41 +0600

    product price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Cart = (props) => {
   const cart = props.cart;
+  const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+
   return (
     <div>
       <h4>Order Summery</h4>
       <p>Item Ordered: {cart.length}</p>
+      <p>Total Price: {totalPrice}</p>
     </div>
   );
 };
