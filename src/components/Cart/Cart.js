6413450e4ea5a1b9f5c1b68bd4ca50bb72d4e1aa
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 19:31:43 +0600

    product cart using for loop

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Cart = (props) => {
   const cart = props.cart;
-  const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+  // const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+  let totalPrice = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const product = cart[i];
+    totalPrice = totalPrice + product.price;
+  }
 
   return (
     <div>
