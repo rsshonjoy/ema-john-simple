a8ea34fddd3b32f7bf64ae930769351c85d13ea5
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-26 20:05:51 +0600

    cart complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,11 +9,32 @@ const Cart = (props) => {
     totalPrice = totalPrice + product.price;
   }
 
+  let shipping = 0;
+  if (totalPrice > 35) {
+    shipping = 0;
+  }else if (totalPrice > 15) {
+    shipping = 4.99;
+  }else if (totalPrice > 0) {
+    shipping = 12.99;
+  }
+
+  const tax = totalPrice / 10;
+
+  const grandTotal = totalPrice + shipping + Number(tax);
+
+  const formatNumber = num => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+  }
+
   return (
     <div>
       <h4>Order Summery</h4>
       <p>Item Ordered: {cart.length}</p>
-      <p>Total Price: {totalPrice}</p>
+      <p>Product Price: {formatNumber(totalPrice)}</p>
+      <p><small>Shipping Cost: {shipping}</small></p>
+      <p><small>Tax + VAT: {formatNumber(tax)}</small></p>
+      <p>Total Price: {formatNumber(grandTotal)}</p>
     </div>
   );
 };
