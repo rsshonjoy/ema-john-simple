44491a82165bf916a07cbc6048d5f137cf5ab57c
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-04 19:41:41 +0600

    set quantity

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
 import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
   const {cart, setCart} = useState([]);
   useEffect(() => {
     const saveCart = getDatabaseCart();
-    console.log(saveCart);
+    const productKeys =Object.keys(saveCart);
+    const cartProducts = productKeys.map(key => {
+      const product = fakeData.find(pd =>pd.key === key);
+      product.quantity = saveCart[key];
+      return product;
+    });
+    console.log(cartProducts);
     
   })
   return (
