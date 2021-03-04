6e5ebd7b14b09a6e092b312ff2c5b073d024f2bf
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-04 18:22:12 +0600

    display clicked data

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 
 const ProductDetail = () => {
   const {productKey} = useParams();
+  const product = fakeData.find(pd => pd.key === productKey);
+  console.log(product);
+  
   return (
     <div>
       <h1>{productKey} Details</h1>
+      <Product product={product}></Product>
     </div>
   );
 };
