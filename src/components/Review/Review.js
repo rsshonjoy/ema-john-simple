3a3edcd15f07c0ea31bee1c1fc611c4ae01fad3d
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-05 09:01:07 +0600

    remove product in display

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,7 +6,8 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 const Review = () => {
   const [cart, setCart] = useState([]);
   const removeProduct = (productKey) => {
-    console.log('remove clicked', productKey);
+    const newCart = cart.filter(pd => pd.key !== productKey);
+    setCart(newCart);
   }
   useEffect(() => {
     const saveCart = getDatabaseCart();
