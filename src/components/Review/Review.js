8ee5d59b5b8eeeb9c2a4b1fe48473f2e7df23c7e
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-05 18:58:52 +0600

    Please Order, Clear state

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
@@ -8,8 +8,8 @@ const Review = () => {
   const [cart, setCart] = useState([]);
 
   const handlePlaceOrder = () => {
-    console.log('order placed');
-    
+    setCart([]);
+    processOrder();
   }
   const removeProduct = (productKey) => {
     const newCart = cart.filter(pd => pd.key !== productKey);
